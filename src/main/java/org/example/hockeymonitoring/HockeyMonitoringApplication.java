2c51fbe802ff07b862c43ce0634a368495522a1e
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-09-18 21:21:35 +0500

    initial commit

---
+++ src/main/java/org/example/hockeymonitoring/HockeyMonitoringApplication.java
@@ -0,0 +1,13 @@
+package org.example.hockeymonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HockeyMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hockeymonitoring/HockeyMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hockeymonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
