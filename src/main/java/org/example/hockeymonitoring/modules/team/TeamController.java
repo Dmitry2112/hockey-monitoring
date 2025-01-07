ed7fdf16244548bd11b0e0716e33fd89cee56cac
Dmitry <dmitriy_kozachenko_02@mail.ru>
2025-01-07 18:47:25 +0500

    добавил обертку для ответов в TeamController

--- src/main/java/org/example/hockeymonitoring/modules/team/TeamController.java
+++ src/main/java/org/example/hockeymonitoring/modules/team/TeamController.java
@@ -3,6 +3,7 @@ package org.example.hockeymonitoring.modules.team;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.example.hockeymonitoring.modules.team.dto.AddAthletesDto;
+import org.example.hockeymonitoring.shared.Response;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -16,32 +17,38 @@ public class TeamController {
     private final TeamService teamService;
 
     @GetMapping
-    public List<Team> getList() {
-        return teamService.getList();
+    public Response<List<Team>> getList() {
+        List<Team> teams = teamService.getList();
+        return new Response<>(teams);
     }
 
     @GetMapping("/{id}")
-    public Team getOne(@PathVariable Long id) {
-        return teamService.getOne(id);
+    public Response<Team> getOne(@PathVariable Long id) {
+        Team team = teamService.getOne(id);
+        return new Response<>(team);
     }
 
     @PostMapping
-    public Team create(@RequestBody Team team) {
-        return teamService.create(team);
+    public Response<Team> create(@RequestBody Team team) {
+        Team createdTeam = teamService.create(team);
+        return new Response<>(createdTeam);
     }
 
     @PatchMapping("/{id}")
-    public Team patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
-        return teamService.patch(id, patchNode);
+    public Response<Team> patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        Team team = teamService.patch(id, patchNode);
+        return new Response<>(team);
     }
 
     @DeleteMapping("/{id}")
-    public Team delete(@PathVariable Long id) {
-        return teamService.delete(id);
+    public Response<Team> delete(@PathVariable Long id) {
+        Team team = teamService.delete(id);
+        return new Response<>(team);
     }
 
     @PatchMapping("/{id}/add-athletes")
-    public Team addAthletes(@PathVariable Long id, @RequestBody AddAthletesDto dto) {
-        return teamService.addAthletes(id, dto);
+    public Response<Team> addAthletes(@PathVariable Long id, @RequestBody AddAthletesDto dto) {
+        Team team = teamService.addAthletes(id, dto);
+        return new Response<>(team);
     }
 }
