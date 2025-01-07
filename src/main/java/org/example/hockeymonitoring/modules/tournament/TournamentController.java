eb723ac2c3574d35c1c7f594112325cda272c278
Dmitry <dmitriy_kozachenko_02@mail.ru>
2025-01-07 18:53:54 +0500

    добавил обертку для ответов в TournamentController

--- src/main/java/org/example/hockeymonitoring/modules/tournament/TournamentController.java
+++ src/main/java/org/example/hockeymonitoring/modules/tournament/TournamentController.java
@@ -3,6 +3,7 @@ package org.example.hockeymonitoring.modules.tournament;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.example.hockeymonitoring.modules.tournament.dto.CreateTournamentDto;
+import org.example.hockeymonitoring.shared.Response;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -16,27 +17,32 @@ public class TournamentController {
     private final TournamentService tournamentService;
 
     @GetMapping
-    public List<Tournament> getList() {
-        return tournamentService.getList();
+    public Response<List<Tournament>> getList() {
+        List<Tournament> tournaments = tournamentService.getList();
+        return new Response<>(tournaments);
     }
 
     @GetMapping("/{id}")
-    public Tournament getOne(@PathVariable Long id) {
-        return tournamentService.getOne(id);
+    public Response<Tournament> getOne(@PathVariable Long id) {
+        Tournament tournament = tournamentService.getOne(id);
+        return new Response<>(tournament);
     }
 
     @PostMapping
-    public Tournament create(@RequestBody CreateTournamentDto dto) {
-        return tournamentService.create(dto);
+    public Response<Tournament> create(@RequestBody CreateTournamentDto dto) {
+        Tournament tournament = tournamentService.create(dto);
+        return new Response<>(tournament);
     }
 
     @PatchMapping("/{id}")
-    public Tournament patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
-        return tournamentService.patch(id, patchNode);
+    public Response<Tournament> patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        Tournament tournament = tournamentService.patch(id, patchNode);
+        return new Response<>(tournament);
     }
 
     @DeleteMapping("/{id}")
-    public Tournament delete(@PathVariable Long id) {
-        return tournamentService.delete(id);
+    public Response<Tournament> delete(@PathVariable Long id) {
+        Tournament tournament = tournamentService.delete(id);
+        return new Response<>(tournament);
     }
 }
