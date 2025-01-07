98f27ce2ba83fd8383ae65c05471127298f723d0
Dmitry <dmitriy_kozachenko_02@mail.ru>
2025-01-07 18:32:59 +0500

    добавил обертку для ответов в CategoryController

--- src/main/java/org/example/hockeymonitoring/modules/category/CategoryController.java
+++ src/main/java/org/example/hockeymonitoring/modules/category/CategoryController.java
@@ -2,6 +2,7 @@ package org.example.hockeymonitoring.modules.category;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
+import org.example.hockeymonitoring.shared.Response;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -15,27 +16,32 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getList() {
-        return categoryService.getList();
+    public Response<List<Category>> getList() {
+        List<Category> categories = categoryService.getList();
+        return new Response<>(categories);
     }
 
     @GetMapping("/{id}")
-    public Category getOne(@PathVariable Long id) {
-        return categoryService.getOne(id);
+    public Response<Category> getOne(@PathVariable Long id) {
+        Category category = categoryService.getOne(id);
+        return new Response<>(category);
     }
 
     @PostMapping
-    public Category create(@RequestBody Category category) {
-        return categoryService.create(category);
+    public Response<Category> create(@RequestBody Category category) {
+        Category createdCategory = categoryService.create(category);
+        return new Response<>(createdCategory);
     }
 
     @PatchMapping("/{id}")
-    public Category patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
-        return categoryService.patch(id, patchNode);
+    public Response<Category> patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        Category category = categoryService.patch(id, patchNode);
+        return new Response<>(category);
     }
 
     @DeleteMapping("/{id}")
-    public Category deleteCategory(@PathVariable Long id) {
-        return categoryService.delete(id);
+    public Response<Category> deleteCategory(@PathVariable Long id) {
+        Category category = categoryService.delete(id);
+        return new Response<>(category);
     }
 }
