02ec4a8884bb8032a1e653b3c707298c64b5a13e
Dmitry <dmitriy_kozachenko_02@mail.ru>
2025-01-09 17:35:57 +0500

    настроил cors

--- src/main/java/org/example/hockeymonitoring/HockeyMonitoringApplication.java
+++ src/main/java/org/example/hockeymonitoring/HockeyMonitoringApplication.java
@@ -2,6 +2,9 @@ package org.example.hockeymonitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HockeyMonitoringApplication {
@@ -10,4 +13,15 @@ public class HockeyMonitoringApplication {
         SpringApplication.run(HockeyMonitoringApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD");
+            }
+        };
+    }
 }
